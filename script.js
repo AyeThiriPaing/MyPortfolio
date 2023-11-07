e6fa412f84a063104514d6a7b39058705e93941e
AyeThiriPaing <124244936+AyeThiriPaing@users.noreply.github.com>
2023-11-07 16:44:17 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
+
+var element = new Typed('#element', {
+    strings: [
+        'Developer',
+        'Programmer',
+        'UI/UX Designer'
+    ],
+    typeSpeed: 0,
+    backSpeed: 0,
+    loop: true
+  });
+    
